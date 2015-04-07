63e87f9990e0ba1b6a13eab22f0dbc5fbb8d1227
szakrzewski <simon.zakrzewski@gmail.com>
2015-04-07 11:39:00 -0500

    Lawn Mowing App

---
+++ Lawn Mowing Service/Lawn Mowing Service/LawnJobPricer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawn_Mowing_Service
+{
+    class LawnJobPricer
+    {
+        private int length;
+        private int width;
+        private int sqFootage;
+        private int weeklyPrice;
+ 
+
+            public void LawnCalculator()
+        {
+
+            Console.WriteLine("Please enter the length of your yard");
+            length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the width of your yard");
+            width = int.Parse(Console.ReadLine());
+
+            sqFootage = length*width;
+
+            if (sqFootage < 400)
+            {
+                weeklyPrice = 25;
+            }
+
+            else if (sqFootage >= 400 && sqFootage < 600)
+            {
+                weeklyPrice = 35;
+            }
+
+            else
+            {
+                weeklyPrice = 50;
+            }
+
+            Console.WriteLine("Your weekly fee is ${0}", weeklyPrice);
+            Console.WriteLine("Your total fee for the 20 week season is ${0}", 20*weeklyPrice);
+        }
+    }
+}
---
+++ Lawn Mowing Service/Lawn Mowing Service/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawn_Mowing_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LawnJobPricer jobCalc = new LawnJobPricer();
+            jobCalc.LawnCalculator();
+
+        }
+    }
+}
